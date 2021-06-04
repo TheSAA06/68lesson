ee029a730919ce9139359220864da782b2d96e7e
TheSAA06 <60203764+TheSAA06@users.noreply.github.com>
2021-06-04 09:10:25 +0900

    Add files via upload

---
+++ move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    Transform tr;
+    float ad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ad =Input.GetAxis("Horizontal");
+        tr.Rotate(0,ad,0);
+    }
+}
